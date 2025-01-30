fe31c4ffdca1a0eb99f135fed88a8ac37d7fee21
SIDTHEKID2 <iamnotsidkarthi@gmail.com>
2025-01-29 19:27:13 -0500

    initializations

--- src/main/java/frc/robot/commands/IntakingFromTheSource.java
+++ src/main/java/frc/robot/commands/IntakingFromTheSource.java
@@ -5,9 +5,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.flywheeler.Flywheeler;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class IntakingFromTheSource extends Command {
+
+  private final Flywheel flywheel;
+
   /** Creates a new IntakingFromTheSource. */
   public IntakingFromTheSource() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -15,7 +19,11 @@ public class IntakingFromTheSource extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+
+    DigitalInput input = new DigitalInput(0);
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
