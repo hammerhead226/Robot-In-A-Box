fc99650c7fce66096ae2c44087bdd088ed74ecdb
David Chen <dychen2523@gmail.com>
2025-02-12 18:34:45 -0500

    added approachreefperpendicular file

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 149;
-  public static final String GIT_SHA = "cea35cea69ae9fdc4f2aaaceda22215ae74a19d2";
-  public static final String GIT_DATE = "2025-02-10 18:35:23 EST";
+  public static final int GIT_REVISION = 150;
+  public static final String GIT_SHA = "db5cda18349bc794f55883254fadae1215b6e739";
+  public static final String GIT_DATE = "2025-02-10 19:42:37 EST";
   public static final String GIT_BRANCH = "drive-command-refactor";
-  public static final String BUILD_DATE = "2025-02-10 19:32:10 EST";
-  public static final long BUILD_UNIX_TIME = 1739233930925L;
-  public static final int DIRTY = 128;
+  public static final String BUILD_DATE = "2025-02-12 18:29:06 EST";
+  public static final long BUILD_UNIX_TIME = 1739402946430L;
+  public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/commands/ApproachReefPerpendicular.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.List;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ApproachReefPerpendicular extends Command {
+   private final Drive drive;
+  boolean finished;
+  Command pathCommand;
+  /** Creates a new ApproachReefPerpendicular. */
+  public ApproachReefPerpendicular(Drive drive) {
+    this.drive = drive;
+    finished = false;
+    addRequirements(drive);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+      Pose2d targetPose =
+        new Pose2d(
+            drive.getNearestSide().getTranslation().minus(drive.getOffset()),
+            drive.getNearestSide().getRotation());
+    // new Pose2d(nearestSide.getTranslation().minus(offset), nearestSide.getRotation())
+    List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(drive.getPose(), targetPose);
+
+    PathPlannerPath path =
+        new PathPlannerPath(
+            waypoints,
+            new PathConstraints(3.5, 2.7, 100, 180), // these numbers from last year's code
+            null, // The ideal starting state, this is only relevant for pre-planned paths, so can
+            // be null for on-the-fly paths.
+            new GoalEndState(0.5, targetPose.getRotation()));
+    path.preventFlipping = true;
+
+    pathCommand = AutoBuilder.followPath(path);
+    pathCommand.initialize();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    pathCommand.execute();
+
+    finished = pathCommand.isFinished();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    pathCommand.cancel();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
