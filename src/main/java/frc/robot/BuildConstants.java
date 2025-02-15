d76c89afa7389b7f0feb54bf10a5ee69bafa88ad
ThePaniniMan <prmaringantiacts@gmail.com>
2025-02-14 19:44:56 -0500

    swapped score and added wait

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 177;
-  public static final String GIT_SHA = "34a43bf81cbee181fc87678cac19773d3b9d2a4e";
-  public static final String GIT_DATE = "2025-02-14 18:37:38 EST";
+  public static final int GIT_REVISION = 178;
+  public static final String GIT_SHA = "3efd51f7aa506799e543ef84a58bb28d050ceff1";
+  public static final String GIT_DATE = "2025-02-14 19:35:35 EST";
   public static final String GIT_BRANCH = "refactor-autons";
-  public static final String BUILD_DATE = "2025-02-14 19:31:10 EST";
-  public static final long BUILD_UNIX_TIME = 1739579470898L;
+  public static final String BUILD_DATE = "2025-02-14 19:44:04 EST";
+  public static final long BUILD_UNIX_TIME = 1739580244141L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
