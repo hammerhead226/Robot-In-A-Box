43f63d7d931f32df7fe53d99e98821a765510934
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-18 21:02:55 -0500

    Added a BlueMiddleRight
    Renamed 1.1 to BlueMiddleLeft which implies this is what we do when we are blue starting in the middle using the left source. And also created a BlueMiddleRight which does the same but with the right source.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 179;
-  public static final String GIT_SHA = "d76c89afa7389b7f0feb54bf10a5ee69bafa88ad";
-  public static final String GIT_DATE = "2025-02-14 19:44:56 EST";
+  public static final int GIT_REVISION = 180;
+  public static final String GIT_SHA = "321970458a43f0b0bd0b52084aab4a7277f1999a";
+  public static final String GIT_DATE = "2025-02-18 20:41:53 EST";
   public static final String GIT_BRANCH = "refactor-autons";
-  public static final String BUILD_DATE = "2025-02-18 20:39:48 EST";
-  public static final long BUILD_UNIX_TIME = 1739929188551L;
+  public static final String BUILD_DATE = "2025-02-18 21:01:00 EST";
+  public static final long BUILD_UNIX_TIME = 1739930460732L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
