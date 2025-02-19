321970458a43f0b0bd0b52084aab4a7277f1999a
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-18 20:41:53 -0500

    Fixed 1.1 Auton so it aligns with our new robot design
    Fixed 1.1 Auton so it aligns with our new robot design

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 178;
-  public static final String GIT_SHA = "3efd51f7aa506799e543ef84a58bb28d050ceff1";
-  public static final String GIT_DATE = "2025-02-14 19:35:35 EST";
+  public static final int GIT_REVISION = 179;
+  public static final String GIT_SHA = "d76c89afa7389b7f0feb54bf10a5ee69bafa88ad";
+  public static final String GIT_DATE = "2025-02-14 19:44:56 EST";
   public static final String GIT_BRANCH = "refactor-autons";
-  public static final String BUILD_DATE = "2025-02-14 19:44:04 EST";
-  public static final long BUILD_UNIX_TIME = 1739580244141L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-18 20:39:48 EST";
+  public static final long BUILD_UNIX_TIME = 1739929188551L;
+  public static final int DIRTY = 128;
 
   private BuildConstants() {}
 }
