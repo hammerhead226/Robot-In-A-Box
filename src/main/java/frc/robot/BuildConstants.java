cca1eadf9b54ead0ede2bee0e2f87ed9c7aceb40
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-19 20:14:13 -0500

    Blue Right Faced Wrong Way
    Blue right faces the wrong way but fixed now.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 182;
-  public static final String GIT_SHA = "347ff302daa25b34b58822de4e57d1be3807124c";
-  public static final String GIT_DATE = "2025-02-19 19:43:38 EST";
+  public static final int GIT_REVISION = 183;
+  public static final String GIT_SHA = "5c9a1c0f9234e8c31e99e1218d9bfe2462099473";
+  public static final String GIT_DATE = "2025-02-19 19:50:18 EST";
   public static final String GIT_BRANCH = "refactor-autons";
-  public static final String BUILD_DATE = "2025-02-19 19:48:00 EST";
-  public static final long BUILD_UNIX_TIME = 1740012480499L;
+  public static final String BUILD_DATE = "2025-02-19 20:12:01 EST";
+  public static final long BUILD_UNIX_TIME = 1740013921699L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
