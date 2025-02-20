2320f907ef9ac03ddee0c6cde9324b5f2a516220
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-19 21:58:43 -0500

    Switched Direction Robot Faces
    I now realize that we intake from the right side and deposit from the left, I previously had this the other way around and fiexd it.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 183;
-  public static final String GIT_SHA = "5c9a1c0f9234e8c31e99e1218d9bfe2462099473";
-  public static final String GIT_DATE = "2025-02-19 19:50:18 EST";
+  public static final int GIT_REVISION = 185;
+  public static final String GIT_SHA = "2ecda7f42a71cf40b0763e1809f34167b9b396db";
+  public static final String GIT_DATE = "2025-02-19 21:35:24 EST";
   public static final String GIT_BRANCH = "refactor-autons";
-  public static final String BUILD_DATE = "2025-02-19 20:12:01 EST";
-  public static final long BUILD_UNIX_TIME = 1740013921699L;
+  public static final String BUILD_DATE = "2025-02-19 21:54:48 EST";
+  public static final long BUILD_UNIX_TIME = 1740020088852L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
