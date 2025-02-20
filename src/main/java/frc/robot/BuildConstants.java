5c9a1c0f9234e8c31e99e1218d9bfe2462099473
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-19 19:50:18 -0500

    Fixed Bug
    Fixed a very minor bug in RedMIddleLeft

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 181;
-  public static final String GIT_SHA = "43f63d7d931f32df7fe53d99e98821a765510934";
-  public static final String GIT_DATE = "2025-02-18 21:02:55 EST";
+  public static final int GIT_REVISION = 182;
+  public static final String GIT_SHA = "347ff302daa25b34b58822de4e57d1be3807124c";
+  public static final String GIT_DATE = "2025-02-19 19:43:38 EST";
   public static final String GIT_BRANCH = "refactor-autons";
-  public static final String BUILD_DATE = "2025-02-19 19:39:42 EST";
-  public static final long BUILD_UNIX_TIME = 1740011982490L;
+  public static final String BUILD_DATE = "2025-02-19 19:48:00 EST";
+  public static final long BUILD_UNIX_TIME = 1740012480499L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
