347ff302daa25b34b58822de4e57d1be3807124c
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-02-19 19:43:38 -0500

    Created A Bunch of new autons for red side
    Added a blueright, blueleft, redleft, redright, redmiddleleft, and redmiddleright. Also made it so our robot places sidewyas just like it does irl.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 180;
-  public static final String GIT_SHA = "321970458a43f0b0bd0b52084aab4a7277f1999a";
-  public static final String GIT_DATE = "2025-02-18 20:41:53 EST";
+  public static final int GIT_REVISION = 181;
+  public static final String GIT_SHA = "43f63d7d931f32df7fe53d99e98821a765510934";
+  public static final String GIT_DATE = "2025-02-18 21:02:55 EST";
   public static final String GIT_BRANCH = "refactor-autons";
-  public static final String BUILD_DATE = "2025-02-18 21:01:00 EST";
-  public static final long BUILD_UNIX_TIME = 1739930460732L;
+  public static final String BUILD_DATE = "2025-02-19 19:39:42 EST";
+  public static final long BUILD_UNIX_TIME = 1740011982490L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
