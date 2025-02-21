693b0f5198f9cc0925d58266b32dbd761e0e4542
David Chen <dychen2523@gmail.com>
2025-02-20 21:32:09 -0500

    switched back to 226 team num

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 205;
-  public static final String GIT_SHA = "fe37b158410977733a57dc5f8a72fb6afb66f5b0";
-  public static final String GIT_DATE = "2025-02-20 19:11:53 EST";
+  public static final int GIT_REVISION = 206;
+  public static final String GIT_SHA = "9c2c6b55bc6f3fd659f8612abd9a96b9c1a1b527";
+  public static final String GIT_DATE = "2025-02-20 21:17:12 EST";
   public static final String GIT_BRANCH = "superstructure-refactor";
-  public static final String BUILD_DATE = "2025-02-20 20:06:43 EST";
-  public static final long BUILD_UNIX_TIME = 1740100003131L;
+  public static final String BUILD_DATE = "2025-02-20 21:30:40 EST";
+  public static final long BUILD_UNIX_TIME = 1740105040246L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
