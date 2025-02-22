4b6b59304bf6ec86c443b66dda65732dd59c4d31
David Chen <dychen2523@gmail.com>
2025-02-22 14:32:21 -0500

    scoral arm tuned

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 289;
-  public static final String GIT_SHA = "327f917dd59f7589e0ffafa413029661322f62db";
-  public static final String GIT_DATE = "2025-02-22 02:34:24 EST";
+  public static final int GIT_REVISION = 290;
+  public static final String GIT_SHA = "c41e0dce7bf5f4501b94e6ad4964e537cc7d1a77";
+  public static final String GIT_DATE = "2025-02-22 12:00:56 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-22 11:59:13 EST";
-  public static final long BUILD_UNIX_TIME = 1740243553049L;
+  public static final String BUILD_DATE = "2025-02-22 14:29:57 EST";
+  public static final long BUILD_UNIX_TIME = 1740252597671L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
