c0e97aadb2b3564f3bcad812cf2e989271f9cb27
David Chen <dychen2523@gmail.com>
2025-02-23 17:00:07 -0500

    renamed file names and change left right offset

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 313;
-  public static final String GIT_SHA = "00363b416ca235a82c70b0f27673fade1b79ee54";
-  public static final String GIT_DATE = "2025-02-23 16:40:11 EST";
+  public static final int GIT_REVISION = 314;
+  public static final String GIT_SHA = "cb2ad7c298f64e56a844b295cf7dd53455acc225";
+  public static final String GIT_DATE = "2025-02-23 16:46:11 EST";
   public static final String GIT_BRANCH = "pre-lake-city";
-  public static final String BUILD_DATE = "2025-02-23 16:41:47 EST";
-  public static final long BUILD_UNIX_TIME = 1740346907920L;
+  public static final String BUILD_DATE = "2025-02-23 16:56:12 EST";
+  public static final long BUILD_UNIX_TIME = 1740347772729L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
