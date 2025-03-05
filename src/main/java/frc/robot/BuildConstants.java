4d420f14a0f04b3f13d81a3d0791a95653147c36
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-03-04 19:50:33 -0500

    Attempted 3 Coral Auton
    Still having some issues with timing and not showing up in shuffleboard so hoping that committing will fix the issue.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 384;
-  public static final String GIT_SHA = "924d7f19493e92099f7fd699570bd1960fb4bb21";
-  public static final String GIT_DATE = "2025-03-02 10:24:11 EST";
-  public static final String GIT_BRANCH = "pre-lake-city";
-  public static final String BUILD_DATE = "2025-03-02 15:51:36 EST";
-  public static final long BUILD_UNIX_TIME = 1740948696329L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 385;
+  public static final String GIT_SHA = "0edae7ed07d9d544a891a614eafd39d36d407329";
+  public static final String GIT_DATE = "2025-03-02 20:04:51 EST";
+  public static final String GIT_BRANCH = "3-Coral-Autons";
+  public static final String BUILD_DATE = "2025-03-04 19:48:08 EST";
+  public static final long BUILD_UNIX_TIME = 1741135688923L;
+  public static final int DIRTY = 128;
 
   private BuildConstants() {}
 }
