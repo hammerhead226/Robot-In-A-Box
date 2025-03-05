08c298645f2f321427fe4ab31b1a59a993090ba5
NootSquared <prmaringantiacts@gmail.com>
2025-03-04 20:29:52 -0500

    CONSTANT REEF ROTATION TEST

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 384;
-  public static final String GIT_SHA = "924d7f19493e92099f7fd699570bd1960fb4bb21";
-  public static final String GIT_DATE = "2025-03-02 10:24:11 EST";
-  public static final String GIT_BRANCH = "pre-lake-city";
-  public static final String BUILD_DATE = "2025-03-02 15:51:36 EST";
-  public static final long BUILD_UNIX_TIME = 1740948696329L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 386;
+  public static final String GIT_SHA = "974f618d0ed75977f47bea19ab0faedeb5c2fb8d";
+  public static final String GIT_DATE = "2025-03-04 19:21:14 EST";
+  public static final String GIT_BRANCH = "handoff-improvement";
+  public static final String BUILD_DATE = "2025-03-04 20:12:10 EST";
+  public static final long BUILD_UNIX_TIME = 1741137130559L;
+  public static final int DIRTY = 128;
 
   private BuildConstants() {}
 }
--- src/main/java/frc/robot/commands/ApproachReefPerpendicular.java
+++ src/main/java/frc/robot/commands/ApproachReefPerpendicular.java
@@ -13,7 +13,6 @@ import com.pathplanner.lib.path.Waypoint;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.SubsystemConstants;
 import frc.robot.subsystems.SuperStructure;
