948c2442f71211c83078582f13ddd9ca69377453
Rushi Jariwala <147006470+AlpacaCoder1@users.noreply.github.com>
2025-03-07 19:26:41 -0500

    optimized auton to achieve 3-coral autos (super close to 3 corals in 15 but not there yet)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 386;
-  public static final String GIT_SHA = "4d420f14a0f04b3f13d81a3d0791a95653147c36";
-  public static final String GIT_DATE = "2025-03-04 19:50:33 EST";
+  public static final int GIT_REVISION = 388;
+  public static final String GIT_SHA = "062e968d3b03ea0de535a5cb74a05a2d540cae45";
+  public static final String GIT_DATE = "2025-03-06 20:58:32 EST";
   public static final String GIT_BRANCH = "3-Coral-Autons";
-  public static final String BUILD_DATE = "2025-03-06 20:53:00 EST";
-  public static final long BUILD_UNIX_TIME = 1741312380320L;
+  public static final String BUILD_DATE = "2025-03-07 18:26:16 EST";
+  public static final long BUILD_UNIX_TIME = 1741389976213L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
