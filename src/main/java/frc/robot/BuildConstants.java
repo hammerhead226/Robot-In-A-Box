261b4d04ccb6fe9976b775a3cf43349a6b3bf2ae
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-03-08 13:10:21 -0500

    4-1-4 Auton Working on BlueLeft
    We got the 4-1-4 auton to work on BlueLeft and now we are going to work on getting it to 4-4-4.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 388;
-  public static final String GIT_SHA = "062e968d3b03ea0de535a5cb74a05a2d540cae45";
-  public static final String GIT_DATE = "2025-03-06 20:58:32 EST";
+  public static final int GIT_REVISION = 389;
+  public static final String GIT_SHA = "948c2442f71211c83078582f13ddd9ca69377453";
+  public static final String GIT_DATE = "2025-03-07 19:26:41 EST";
   public static final String GIT_BRANCH = "3-Coral-Autons";
-  public static final String BUILD_DATE = "2025-03-07 18:26:16 EST";
-  public static final long BUILD_UNIX_TIME = 1741389976213L;
+  public static final String BUILD_DATE = "2025-03-08 13:08:23 EST";
+  public static final long BUILD_UNIX_TIME = 1741457303884L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
