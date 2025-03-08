2ba46c11be03488d952f7f5d92b15252c9ae4801
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-03-08 14:57:20 -0500

    BlueMiddleRight 4-4-4
    BlueMiddleRight 4-4-4

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 393;
-  public static final String GIT_SHA = "cbda00f9268e47dcca733a39681a7789982c28d2";
-  public static final String GIT_DATE = "2025-03-08 14:32:59 EST";
+  public static final int GIT_REVISION = 394;
+  public static final String GIT_SHA = "b069d389dbb2e0cd5297e9679ab881f9550fb6cc";
+  public static final String GIT_DATE = "2025-03-08 14:45:45 EST";
   public static final String GIT_BRANCH = "3-Coral-Autons";
-  public static final String BUILD_DATE = "2025-03-08 14:44:49 EST";
-  public static final long BUILD_UNIX_TIME = 1741463089102L;
+  public static final String BUILD_DATE = "2025-03-08 14:55:07 EST";
+  public static final long BUILD_UNIX_TIME = 1741463707094L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
