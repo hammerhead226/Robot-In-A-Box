8ab5f7b9563a0abfe58e9fd1e68fa94736061181
ArnavGuptaOfficial <arnavgupta045@gmail.com>
2025-03-11 19:22:23 -0400

    Switched SCORE_CORAL to SCORE_CORAL_NEW
    There was a bug where all of the score coral new was switched to score coral, that issue should be resolved now.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 397;
-  public static final String GIT_SHA = "4f7e215f0326a80b28bc0e181f8689e151444c9e";
-  public static final String GIT_DATE = "2025-03-10 18:53:55 EDT";
+  public static final int GIT_REVISION = 398;
+  public static final String GIT_SHA = "b66466d17e13ddab009662fa207ba694891baa32";
+  public static final String GIT_DATE = "2025-03-10 19:30:47 EDT";
   public static final String GIT_BRANCH = "3-Coral-Autons";
-  public static final String BUILD_DATE = "2025-03-10 19:01:18 EDT";
-  public static final long BUILD_UNIX_TIME = 1741647678376L;
+  public static final String BUILD_DATE = "2025-03-11 19:21:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1741735260372L;
   public static final int DIRTY = 128;
 
   private BuildConstants() {}
