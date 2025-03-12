1b6165d31ea2131cb11781d3d9084d07dc9466ce
David Chen <dychen2523@gmail.com>
2025-03-12 11:20:31 -0400

    switching branch

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 409;
-  public static final String GIT_SHA = "fcf0bfed1744cbc818de7c8a92e3a91c290c531a";
-  public static final String GIT_DATE = "2025-03-11 20:33:27 EDT";
+  public static final int GIT_REVISION = 432;
+  public static final String GIT_SHA = "6714da4028028ca24dfc51140cb690ebd5e0e0f7";
+  public static final String GIT_DATE = "2025-03-12 11:18:23 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-11 21:19:18 EDT";
-  public static final long BUILD_UNIX_TIME = 1741742358640L;
+  public static final String BUILD_DATE = "2025-03-12 11:19:08 EDT";
+  public static final long BUILD_UNIX_TIME = 1741792748108L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
