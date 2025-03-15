5a51ba65fc17a27d09b58a6032981ec670dd3b6a
David Chen <dychen2523@gmail.com>
2025-03-15 18:24:35 -0400

    enabled the wiggle wiggle

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,22 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-<<<<<<< Updated upstream
-  public static final int GIT_REVISION = 456;
-  public static final String GIT_SHA = "f02e691b30ad1a34ab85c79f92e4e535492903b9";
-  public static final String GIT_DATE = "2025-03-15 12:43:32 EDT";
-  public static final String GIT_BRANCH = "Optimizing-Autons";
-  public static final String BUILD_DATE = "2025-03-15 13:36:05 EDT";
-  public static final long BUILD_UNIX_TIME = 1742060165699L;
-=======
-  public static final int GIT_REVISION = 455;
-  public static final String GIT_SHA = "37f32054ce50a446a1f3966ef8fa6b37220f00df";
-  public static final String GIT_DATE = "2025-03-15 11:25:21 EDT";
-  public static final String GIT_BRANCH = "AlignToReefWithCANrange";
-  public static final String BUILD_DATE = "2025-03-15 14:12:06 EDT";
-  public static final long BUILD_UNIX_TIME = 1742062326141L;
->>>>>>> Stashed changes
-  public static final int DIRTY = 128;
+  public static final int GIT_REVISION = 460;
+  public static final String GIT_SHA = "4969947b7904b868cdde2390f66bacc133f95100";
+  public static final String GIT_DATE = "2025-03-15 18:17:49 EDT";
+  public static final String GIT_BRANCH = "auto-changes";
+  public static final String BUILD_DATE = "2025-03-15 18:19:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1742077196255L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
