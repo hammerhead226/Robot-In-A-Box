37f32054ce50a446a1f3966ef8fa6b37220f00df
David Chen <dychen2523@gmail.com>
2025-03-15 11:25:21 -0400

    added constraint zone so when close to reef it doesnt ram into reef

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 453;
-  public static final String GIT_SHA = "a75e90c6e6b0b9d66d5b80db90b0cc055213cf7a";
-  public static final String GIT_DATE = "2025-03-15 00:03:36 EDT";
+  public static final int GIT_REVISION = 454;
+  public static final String GIT_SHA = "fae015ec0376a70ca56a897f29c69a041dd68d65";
+  public static final String GIT_DATE = "2025-03-15 11:12:45 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-15 11:07:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1742051229204L;
+  public static final String BUILD_DATE = "2025-03-15 11:23:54 EDT";
+  public static final long BUILD_UNIX_TIME = 1742052234127L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
