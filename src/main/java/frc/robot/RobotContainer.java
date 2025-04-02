405e4187ae8b56ed74a3c88fc67a9cda9b621215
zachashaw <90483131+zachashaw@users.noreply.github.com>
2025-04-01 20:11:35 -0400

    Renammed the autos.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
@@ -83,7 +82,6 @@ import frc.robot.subsystems.scoral.ScoralRollersIOTalonFX;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIOLimelight;
 import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
-import java.util.Map;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
@@ -393,13 +391,13 @@ public class RobotContainer {
 
     autos = new SendableChooser<>();
 
-    autos.addOption("BlueLeft", AutoBuilder.buildAuto("BlueLeft"));
+    autos.addOption("Processor", AutoBuilder.buildAuto("Processor"));
     // autos.addOption("BlueLeftPush", AutoBuilder.buildAuto("BlueLeftPush"));
     autos.addOption("CenterBarge", AutoBuilder.buildAuto("CenterBarge"));
     autos.addOption("CenterProcessor", AutoBuilder.buildAuto("CenterProcessor"));
     // autos.addOption("BlueMiddleLeft", AutoBuilder.buildAuto("BlueMiddleLeft"));
     // autos.addOption("BlueMiddleRight", AutoBuilder.buildAuto("BlueMiddleRight"));
-    autos.addOption("BlueRight", AutoBuilder.buildAuto("BlueRight"));
+    autos.addOption("Barge", AutoBuilder.buildAuto("Barge"));
 
     // autos.addOption("Wait6BlueLeftL2", AutoBuilder.buildAuto("Wait6BlueLeftL2"));
     // autos.addOption("Wait2BlueLeftL2", AutoBuilder.buildAuto("Wait2BlueLeftL2"));
