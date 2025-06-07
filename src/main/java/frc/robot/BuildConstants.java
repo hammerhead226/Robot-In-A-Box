b42001e298e5730a32fab012aea4f869a53a1139
devinisaperson <177495008+devinisaperson@users.noreply.github.com>
2025-06-07 11:30:08 -0400

    amended ID constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025RobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 544;
-  public static final String GIT_SHA = "ed2108793852c2cc6f9463e40c4cc43c0a1a2096";
-  public static final String GIT_DATE = "2025-04-11 18:02:12 EDT";
+  public static final int GIT_REVISION = 545;
+  public static final String GIT_SHA = "8222d9dae3ec819e334c07e7c07d57506ec1b4b1";
+  public static final String GIT_DATE = "2025-04-11 21:18:23 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-04-11 20:39:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1744418353074L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-06-05 18:44:29 EDT";
+  public static final long BUILD_UNIX_TIME = 1749163469823L;
+  public static final int DIRTY = 128;
 
   private BuildConstants() {}
 }
