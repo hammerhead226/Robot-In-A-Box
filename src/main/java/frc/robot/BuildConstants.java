b62323b1926fca83639201b9a130127cea1d32cd
codertanishrastogi <tanishrastogi2020@gmail.com>
2025-08-30 12:58:17 -0400

    Refactored most of code. Current code is sub-minimal for usecase (included some previous code for future implementation).

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-In-A-Box";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 554;
-  public static final String GIT_SHA = "a0fbca57b47852602ffe1c4f11f772a69d4bc45b";
-  public static final String GIT_DATE = "2025-08-30 11:49:08 EDT";
+  public static final int GIT_REVISION = 555;
+  public static final String GIT_SHA = "9bcb7c2f473b0687fc8ca672b72c62ecdc4d2330";
+  public static final String GIT_DATE = "2025-08-30 11:54:15 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-08-30 11:52:40 EDT";
-  public static final long BUILD_UNIX_TIME = 1756569160382L;
+  public static final String BUILD_DATE = "2025-08-30 12:56:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1756572991952L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/constants/SubsystemConstants.java
+++ src/main/java/frc/robot/constants/SubsystemConstants.java
@@ -15,7 +15,7 @@
 
 package frc.robot.constants;
 
-import frc.robot.util.LoggedTunableNumber;
+// import frc.robot.util.LoggedTunableNumber; - Part of Example
 
 public final class SubsystemConstants {
   public static boolean coralStuckMode = false;
@@ -23,181 +23,25 @@ public final class SubsystemConstants {
   public static final double LOOP_PERIOD_SECONDS = 0.02;
   public static final boolean tuningMode = true;
 
-  // public static final double NEAR_FAR_AT_REEF_OFFSET = -0.47;
-  public static final LoggedTunableNumber NEAR_FAR_AT_REEF_OFFSET =
-      new LoggedTunableNumber("Auto Align/NEAR_FAR_AT_REEF_OFFSET", -0.525); // -0.482;
-  public static final double NEAR_FAR_AWAY_REEF_OFFSET = -0.9;
-  // more neg moves it to the right
-  // public static final double LEFT_RIGHT_BRANCH_OFFSET = -0.015;
-  public static final LoggedTunableNumber CORRECTION_LEFT_BRANCH_OFFSET =
-      new LoggedTunableNumber("Auto Align/LEFT_BRANCH_OFFSET", -0.045);
-  // new LoggedTunableNumber("Auto Align/LEFT_BRANCH_OFFSET", -0.023);
-  public static final LoggedTunableNumber CORRECTION_RIGHT_BRANCH_OFFSET =
-      new LoggedTunableNumber("Auto Align/RIGHT_BRANCH_OFFSET", -0.045);
-  // new LoggedTunableNumber("Auto Align/RIGHT_BRANCH_OFFSET", -0.023);
-  public static final double APPROACH_OFFSET_LEFT_RIGHT_OFFSET = -0.2;
-  public static final double ADJUST_OFFSET_LEFT_RIGHT_OFFSET = -0.07;
+  // ----- EXAMPLE -----
 
-  public static class IntakeConstants {
-    public static final double CURRENT_LIMIT = 40.0;
-    public static final boolean CURRENT_LIMIT_ENABLED = true;
-  }
+  // // public static final double NEAR_FAR_AT_REEF_OFFSET = -0.47;
+  // public static final LoggedTunableNumber NEAR_FAR_AT_REEF_OFFSET =
+  //     new LoggedTunableNumber("Auto Align/NEAR_FAR_AT_REEF_OFFSET", -0.525); // -0.482;
+  // public static final double NEAR_FAR_AWAY_REEF_OFFSET = -0.9;
+  // // more neg moves it to the right
+  // // public static final double LEFT_RIGHT_BRANCH_OFFSET = -0.015;
+  // public static final LoggedTunableNumber CORRECTION_LEFT_BRANCH_OFFSET =
+  //     new LoggedTunableNumber("Auto Align/LEFT_BRANCH_OFFSET", -0.045);
+  // // new LoggedTunableNumber("Auto Align/LEFT_BRANCH_OFFSET", -0.023);
+  // public static final LoggedTunableNumber CORRECTION_RIGHT_BRANCH_OFFSET =
+  //     new LoggedTunableNumber("Auto Align/RIGHT_BRANCH_OFFSET", -0.045);
+  // // new LoggedTunableNumber("Auto Align/RIGHT_BRANCH_OFFSET", -0.023);
+  // public static final double APPROACH_OFFSET_LEFT_RIGHT_OFFSET = -0.2;
+  // public static final double ADJUST_OFFSET_LEFT_RIGHT_OFFSET = -0.07;
 
-  public static final class ShooterConstants {}
-
-  public static class ElevatorConstants {
-    public static final double CURRENT_LIMIT = 40.0;
-    public static final boolean CURRENT_LIMIT_ENABLED = true;
-
-    public static final double STOW_SETPOINT_INCH = -0.02;
-
-    public static final double DEFAULT_THRESHOLD = 0.1;
-
-    public static final double ELEVATOR_GEAR_RATIO = 12.;
-
-    public static final double PROCESSOR_SETPOINT_INCHES = 4;
-
-    public static final double CANRAGE_ZERO_SETPOINT_INCHES = 2;
-
-    // TODO:: NEED TO FIND
-    public static final double L1_SETPOINT_INCHES = 3.8;
-    public static final double L2_SETPOINT_INCHES = 6.8; // 7.5;
-    // public static final double L2_SETPOINT_INCHES = 6.8;
-    // public static final double L3_SETPOINT_INCHES = 14.5;
-    public static final double L3_SETPOINT_INCHES = 13.7; // 14.2;
-    public static final double L4_SETPOINT_INCHES = 27.4;
-    // public static final double L4_SETPOINT_INCHES = 27;
-    public static final double BARGE_SETPOINT = 27.5;
-    public static final double GROUND_CORAL_ALGAE_SETPOINT_INCHES = 0.5;
-  }
-
-  public static final class ClimberConstants {
-    public static final double CURRENT_LIMIT = 35.0;
-    public static final boolean CURRENT_LIMIT_ENABLED = true;
-
-    public static final double DEFAULT_THRESHOLD = 0.1;
-    public static final double ARM_GEAR_RATIO = 1;
-
-    public static final double STOW_SETPOINT_DEG = 108;
-    public static final double DEPLOY_SETPOINT_DEG = -20;
-  }
-
-  public static final class WinchConstants {
-    public static final double STATOR_CURRENT_LIMIT = 60.0;
-  }
-
-  public static class ScoralArmConstants {
-    public static final double CURRENT_LIMIT = 50.0;
-    public static final boolean CURRENT_LIMIT_ENABLED = true;
-
-    public static final double DEFAULT_THRESHOLD = 1;
-    public static final double ARM_GEAR_RATIO = (25.0 / 1) / (14.0 / 32);
-
-    public static final double STOW_SETPOINT_DEG = 93.5;
-    public static final double L1_CORAL_SCORING_SETPOINT_DEG = 98;
-    public static final double L3_CORAL_SCORING_SETPOINT_DEG = 76.5;
-    public static final double L2_CORAL_SCORING_SETPOINT_DEG = 76.5;
-    public static final double L4_CORAL_SCORING_SETPOINT_DEG = 56;
-    public static final double BARGE_BACK_SETPOINT_DEG = 69;
-    public static final double BARGE_FORWARD_SETPOINT_DEG = 125;
-    public static final double PROCESSOR_SETPOINT_DEG = 20;
-    public static final double GROUND_CORAL_ALGAE_SETPOINT_DEG = 38;
-  }
-
-  public static class LEDConstants {
-    public static final int NUMBER_LEDS = 57 + 24 + 13;
-  }
-
-  public static enum LED_STATE {
-    BLUE,
-    RED,
-    YELLOW,
-    GREEN,
-    GREY,
-    PURPLE,
-    PINK_LAVENDER,
-    PAPAYA_ORANGE,
-    FLASHING_GREEN,
-    FLASHING_RED,
-    FLASHING_BLUE,
-    FIRE,
-    OFF
-  }
-
-  public static enum Mode {
-    REAL,
-    SIM,
-    REPLAY
-  }
-
-  public static enum CoralState {
-    DEFAULT,
-    NO_CORAL,
-    SENSOR,
-    CURRENT
-  }
-
-  public static enum AlgaeState {
-    DEFAULT,
-    NO_ALGAE,
-    CURRENT,
-  }
-
-  public static enum ElevatorState {
-    ZERO,
-    STOW,
-    L1,
-    L2,
-    L3,
-    L4,
-    SOURCE,
-    PROCESSOR
-  }
-
-  public static enum ScoralFlywheelState {
-    ZERO,
-    INTAKING_CORAL,
-    INTAKING_ALGAE,
-    SCORING_CORAL,
-    SCORING_ALGAE
-  }
-
-  public static enum ClimberState {
-    STOW,
-    STAGE_ONE,
-    STAGE_TWO,
-    HANG
-  }
-
-  public static enum AlignState {
-    ALIGNING,
-    IN_POSITION
-  }
-
-  public static enum DriveState {
-    FULLSPEED,
-    SLOW
-  }
-
-  public static enum SuperStructureState {
-    // NONE,
-    STOW,
-    EJECT_ALGAE,
-    SOURCE,
-    PROCESSOR,
-    BARGE_EXTEND,
-    BARGE_SCORE,
-    PROCESSOR_SCORE,
-    L1,
-    L2,
-    L3,
-    L4,
-    SCORING_CORAL,
-    L1_SCORING_CORAL,
-    REEF_INTAKE_ALGAE,
-    CORAL_INTAKE_ALGAE,
-    STOW_ALGAE
-  }
-
-  public static final double CORAL_DIST = 4.0; // 1300; // CHANGE THIS
+  // public static class IntakeConstants {
+  //   public static final double CURRENT_LIMIT = 40.0;
+  //   public static final boolean CURRENT_LIMIT_ENABLED = true;
+  // }
 }
